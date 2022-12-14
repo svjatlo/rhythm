04559aaa85882412de454d335eff93704dde4309
svjatlo <inspirationyellow@gmail.com>
2022-12-14 20:29:34 +0300

    rhythm

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use strict"
+
+const reviewsSwiper = document.querySelector('.swiper-reviews');
+
+if (reviewsSwiper) {
+  const swiper = new Swiper('.swiper-reviews', {
+
+    loop: true,
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
+}
